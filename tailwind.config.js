823b1b859854021ca2d88b6ece321ac225de8ebf
unknown <adebanjoabdulquadri@gmail.com>
2026-08-17 16:52:53 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Playball: ['"Playball"', "cursive"],
+      },
+      backgroundImage: {
+        "hero-left": "url('/images/baner-dec-left.png')",
+      },
+    },
+  },
+};
